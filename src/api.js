0e1c2056a5bd0de085794ed9457db783bb1803cd
Gouse Basha <mohammadgous526@gmail.com>
2022-08-10 13:13:38 +0530

    Fetching-APis

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//  require("dotenv").config();
 //Base URL
 
 const base_url = "https://api.rawg.io/api/";
@@ -29,9 +29,11 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular Games
+const key = process.env.REACT_APP_IGNITE_API_KEY;
+const popular_games = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?key=${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
 
-const popular_games = `games?key=${process.env.IGNITE_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-
-export const popularGamesURL = () => {
-  return `${base_url}${popular_games}`;
-};
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,5 +1,3 @@
-
-
 const initState = {
   popular: [],
   newGames: [],
@@ -10,17 +8,15 @@ const initState = {
 const gamesReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        upcoming: action.payload.upcoming,
+        newGames: action.payload.newGames,
+      };
     default:
       return { ...state };
   }
-};
-
-//Acion Creator
-const fetchGames = (userData) => {
-  return {
-    type: "FETCH_GAMES",
-  };
 };
 
 export default gamesReducer;
