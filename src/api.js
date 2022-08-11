d41285f3c89624c20abfe87974f3e28749865359
Gouse Basha <mohammadgous526@gmail.com>
2022-08-11 18:02:17 +0530

    Adding Games Detail and Games Images

--- src/api.js
+++ src/api.js
@@ -32,8 +32,15 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 const key = process.env.REACT_APP_IGNITE_API_KEY;
 const popular_games = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=12`;
 const upcoming_games = `games?key=${key}&dates=${currentDate},${nextYear}&ordering=-added&page_size=12`;
-const newGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=12`
+const newGames = `games?key=${key}&dates=${lastYear},${currentDate}&ordering=-released&page_size=12`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upComingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+
+// GAME DETAILS
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}.json?&key=${key}`;
+// GAME SCREENSHOTS
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?&key=${key}`;
